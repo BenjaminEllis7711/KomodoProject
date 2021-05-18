bc2e2469889a030b3ada2295972854ad800dfea1
Ben Ellis <Benjamin.Ellis7711@gmail.com>
2021-05-18 15:43:53 -0400

    Added more Unit Test's for Module 1

---
+++ 01_UnitTest/TestsForSectionOne.cs
@@ -0,0 +1,45 @@
+﻿using _01_Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace _01_UnitTest
+{
+    [TestClass]
+    public class TestsForSectionOne
+    {
+        private MenuItem _item = new MenuItem();
+        private MenuRepository _repo = new MenuRepository();
+
+        [TestMethod]
+        public void AddToDirectory_ShouldGetCorrectBool()
+        {
+            MenuItem test = new MenuItem();
+            MenuRepository test2 = new MenuRepository();
+
+            bool addResult = test2.AddItemToDirectory(test);
+
+            Assert.IsTrue(addResult);
+        }
+        [TestMethod]
+        public void GetItemByMealNumber_ShouldGetCorrectItem()
+        {
+            SeedMenuAndTestAddItem_ShouldGetCorrectBool();
+            _item = _repo.GetItemByNumber(1);
+
+            Assert.AreEqual(_item.MealNumber, 1);
+           
+        }
+        [TestMethod]
+        public void SeedMenuAndTestAddItem_ShouldGetCorrectBool()
+        {
+            List<string> seedList = new List<string>();
+            seedList.Add("test");
+            seedList.Add("test2");
+            MenuItem testItem = new MenuItem(1, "Test Meal 1", "Test Description", seedList, 1.50m);
+            bool wasAdded = _repo.AddItemToDirectory(testItem);
+
+            Assert.IsTrue(wasAdded);
+        }
+    }
+}
