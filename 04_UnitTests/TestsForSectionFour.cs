a6b4ef9ee66cb82b72c2c350ddd38ca7ec78737a
Ben Ellis <Benjamin.Ellis7711@gmail.com>
2021-05-20 13:32:48 -0400

    Added Unit Tests for section 4.

--- 04_UnitTests/TestsForSectionFour.cs
+++ 04_UnitTests/TestsForSectionFour.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using _04_Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
 namespace _04_UnitTests
@@ -6,9 +7,34 @@ namespace _04_UnitTests
     [TestClass]
     public class TestsForSectionFour
     {
+        private OutingItem _item = new OutingItem();
+        private OutingRepository _repo = new OutingRepository();
         [TestMethod]
-        public void TestMethod1()
+        public void TestTotalCost_ShouldEqualTotalFromSeed()
         {
+            TestAddAndSeedOutings_ShouldGetCorrectBool();
+            decimal totalCost = _repo.GetTotalCost();
+            Assert.AreEqual(totalCost, 22000);
+        }
+        [TestMethod]
+        public void TestAddAndSeedOutings_ShouldGetCorrectBool()
+        {
+            OutingItem testItem = new OutingItem(EventType.Bowling, 500, new DateTime(2020, 04, 07), 6000.00m);
+            OutingItem testItem2 = new OutingItem(EventType.Concert, 1000, new DateTime(2020, 04, 15), 3500.00m);
+            OutingItem testItem3 = new OutingItem(EventType.Golf, 150, new DateTime(2020, 05, 11), 5000.00m);
+            OutingItem testItem4 = new OutingItem(EventType.Bowling, 150, new DateTime(2020, 04, 11), 7500.00m);
+            bool wasAdded = _repo.AddOuting(testItem);
+            wasAdded = _repo.AddOuting(testItem2);
+            wasAdded = _repo.AddOuting(testItem3);
+            wasAdded = _repo.AddOuting(testItem4);
+            Assert.IsTrue(wasAdded);
+        }
+        [TestMethod]
+        public void TestCostByType_ShouldEqualSeed()
+        {
+            TestAddAndSeedOutings_ShouldGetCorrectBool();
+            decimal costByType = _repo.GetCostByType(1);
+            Assert.AreEqual(costByType, 13500);
         }
     }
 }
